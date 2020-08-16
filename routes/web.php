8e174599290e0b5068e65c8e31b41fb36f5407b1
bvasic <vasicbie@gmail.com>
2020-08-16 12:06:29 +0200

    Laravel From Scratch [Part 3] - Basic Routing & Controllers

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,17 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+// Route::get('/hello', function () {
+//     return "<h1>Hello World<h1>";
+// });
+
+
+Route::get('/', 'PagesController@index');
+
+Route::get('/about', 'PagesController@about');
+
+Route::get('/services', 'PagesController@services');
+
+Route::get('/user/{id}/{name}', function ($id, $name) {
+    return "This is user " .$name. " with an id of " .$id;
 });
